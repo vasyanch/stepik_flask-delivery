120049a7b3b795f80f2bd56b635cf906d51c100b
v.mikhaylov <v.mikhaylov@adventum.ru>
2020-04-30 00:04:44 +0300

    no sqlite

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ current_path = os.path.dirname(os.path.realpath(__file__))
 class Config:
     DEBUG = False
     SECRET_KEY = os.environ.get('STEPIK_DELIVERY_SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db' # os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
